dc919f9f8ed2f6c0ee91006655854159fcd1dcc8
ilhambara <g2a.ilhammubarok@gmail.com>
2022-06-30 03:35:44 +0700

    update: configs

--- config/site.js
+++ config/site.js
@@ -1,7 +1,7 @@
 const siteConfig = {
   socials: {
-    Instagram: "https://www.instagram.com/ofc.xtra/",
-    Discord: "#",
+    Instagram: "https://www.instagram.com/ofc.xtra",
+    Discord: "https://discord.com",
     YouTube: "https://www.youtube.com/channel/UCF8XFs88f6xNLbaRz3DjMBw",
     Steam: "https://steamcommunity.com/groups/xtra10th",
     GitHub: "https://github.com/official-xtra",
