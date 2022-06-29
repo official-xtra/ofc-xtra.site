959416170cf27a3667ac2462d3c9d522581727f4
ilhambara <g2a.ilhammubarok@gmail.com>
2022-06-30 03:05:46 +0700

    update: react@18, next@12, and typescript config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["res.cloudinary.com"],
   },
 };
+
+module.exports = nextConfig;
