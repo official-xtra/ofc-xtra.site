f5ece456a638548d995392bbbf7bc1076ba10202
ilhambara <g2a.ilhammubarok@gmail.com>
2022-07-04 06:41:21 +0700

    update: activities section

--- components/sections/Section-activities.tsx
+++ components/sections/Section-activities.tsx
@@ -1,9 +1,5 @@
 import * as React from "react";
-import {
-  LightMode,
-  useColorMode,
-  useColorModeValue,
-} from "@chakra-ui/color-mode";
+import { LightMode, useColorModeValue } from "@chakra-ui/color-mode";
 import {
   Container,
   Box,
@@ -16,61 +12,73 @@ import {
   useBreakpointValue,
 } from "@chakra-ui/react";
 import { ArrowForwardIcon } from "@chakra-ui/icons";
-import YoutubeEmbed from "../embed/YoutubeEmbed";
+import { YoutubeEmbedMobile } from "../embed/YoutubeEmbed-mobile";
+import { YoutubeEmbedDesktop } from "../embed/YoutubeEmbed-desktop";
 
 export const SectionActivities: React.FC = () => {
-  const boxColor = useColorModeValue("gray.800", "yellow.300");
-
+  const isMobile = useBreakpointValue({ base: true, md: false });
   const buttonSize = useBreakpointValue(["sm", "md", "lg"]);
 
+  const boxColor = useColorModeValue("gray.800", "yellow.300");
+
   return (
-    <Container maxW="container.lg" h="100%" py={4}>
-      <VStack alignItems="center" textAlign="center" spacing={4} pb={[12, 16]}>
+    <Container as="section" maxW="container.lg" h="full" py={4}>
+      <VStack alignItems="center" textAlign="center" py={[12, 16]} spacing={4}>
         <Heading as="h2" fontSize={["2xl", "3xl"]} textColor={boxColor}>
           Activities
         </Heading>
         <Text>We also share some of our activities on our youtube channel</Text>
       </VStack>
 
-      <VStack w="100%" spacing={["4rem", "6rem"]}>
+      <VStack w="full" spacing={[12, 24]}>
         <Flex
-          w="100%"
-          h="100%"
-          justifyContent="flex-start"
+          w="full"
+          h="full"
+          justifyContent={["center", "flex-start"]}
           alignItems="flex-start"
         >
-          <Flex pos="relative" w={["300px", "560px"]} h={["170px", "315px"]}>
-            <Box
-              pos="relative"
-              left="30px"
-              w="100%"
-              h="100%"
-              borderWidth={["2px", "4px"]}
-              borderColor={boxColor}
-              my={[4, 8]}
-            >
-              <YoutubeEmbed embedId="x41OJgZVvmQ" top="-30px" left="-30px" />
-            </Box>
-          </Flex>
+          {isMobile ? (
+            <YoutubeEmbedMobile embedId="x41OJgZVvmQ" />
+          ) : (
+            <Flex pos="relative" w={["300px", "560px"]} h={["170px", "315px"]}>
+              <Box
+                pos="relative"
+                left="30px"
+                w="full"
+                h="full"
+                borderWidth={["2px", "4px"]}
+                borderColor={boxColor}
+                my={[4, 8]}
+              >
+                <YoutubeEmbedDesktop embedId="x41OJgZVvmQ" />
+              </Box>
+            </Flex>
+          )}
         </Flex>
 
-        <Flex w="100%" h="100%" justifyContent="flex-end" alignItems="flex-end">
-          <Flex
-            pos="relative"
-            right="30px"
-            w={["300px", "560px"]}
-            h={["170px", "315px"]}
-          >
-            <Box
-              w="100%"
-              h="100%"
-              borderWidth={["2px", "4px"]}
-              borderColor={boxColor}
-              my={[4, 8]}
-            >
-              <YoutubeEmbed embedId="nQa1vVLBH3k" top="0" right="-30px" />
-            </Box>
-          </Flex>
+        <Flex
+          w="full"
+          h="full"
+          justifyContent={["center", "flex-end"]}
+          alignItems="flex-end"
+        >
+          {isMobile ? (
+            <YoutubeEmbedMobile embedId="nQa1vVLBH3k" />
+          ) : (
+            <Flex pos="relative" w={["300px", "560px"]} h={["170px", "315px"]}>
+              <Box
+                pos="relative"
+                left="30px"
+                w="full"
+                h="full"
+                borderWidth={["2px", "4px"]}
+                borderColor={boxColor}
+                my={[4, 8]}
+              >
+                <YoutubeEmbedDesktop embedId="nQa1vVLBH3k" />
+              </Box>
+            </Flex>
+          )}
         </Flex>
       </VStack>
 
@@ -82,10 +90,10 @@ export const SectionActivities: React.FC = () => {
             rightIcon={<ArrowForwardIcon />}
             size={buttonSize}
             colorScheme="yellow"
-            my={[20, 24]}
+            my={[16, 28]}
             target="_blank"
           >
-            See more activities
+            See other activities
           </Button>
         </Center>
       </LightMode>
